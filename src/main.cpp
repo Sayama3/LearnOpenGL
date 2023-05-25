47b271ea898826710632ff56b7a12432e8b07d36
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-05-25 19:31:52 +0200

    Use OpenGL 4.6 & change window color

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW\glfw3.h>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -26,8 +29,17 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Rendering
     while(!glfwWindowShouldClose(window))
     {
+        // Input Processing
+        processInput(window);
+
+        //TODO: The rendering/ The renderer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -39,4 +51,11 @@ int main() {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    // Simple exit with the "escape" button for now.
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
